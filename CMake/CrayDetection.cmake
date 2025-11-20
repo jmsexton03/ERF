987b93e677dacb377fd13566be867ba460ed1119
Jean M. Sexton <jmsexton@lbl.gov>
2025-11-20 20:32:44 +0000

    fix typo

--- CMake/CrayDetection.cmake
+++ CMake/CrayDetection.cmake
@@ -17,7 +17,7 @@
 # Set Cray context for hierarchical logging
 list(APPEND CMAKE_MESSAGE_CONTEXT "Cray")
 
-if(NOT (ERF_ENABLE_CRAY_AUTO_FIXES OR DEFINED ENV{ERF_ENABLE_CRAY_AUTO_FIXES}))
+if(NOT("${ERF_ENABLE_CRAY_AUTO_FIXES}" OR "$ENV{ERF_ENABLE_CRAY_AUTO_FIXES}"))
     message(STATUS "Auto-fixes disabled by user")
     list(POP_BACK CMAKE_MESSAGE_CONTEXT)
     return()
