69110bfaedc99108391c72e63b1ec8753b349b72
Jean M. Sexton <jmsexton@lbl.gov>
2026-06-03 16:07:35 -0700

    Build routing

--- CMake/BuildERFExe.cmake
+++ CMake/BuildERFExe.cmake
@@ -96,13 +96,19 @@ function(build_erf_lib erf_lib_name)
   ########################## NOAHMP ##################################
   if(ERF_ENABLE_NOAHMP)
     target_include_directories(${erf_lib_name} PUBLIC
-                               $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/Source/LandSurfaceModel/Noah-MP>
-                               $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/Submodules/Noah-MP/drivers/erf>)
+                                $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/Source/LandSurfaceModel/Noah-MP>
+                                $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/Submodules/Noah-MP/drivers/erf>)
     target_sources(${erf_lib_name} PRIVATE
                    ${SRC_DIR}/LandSurfaceModel/Noah-MP/ERF_NOAHMP.cpp)
     target_compile_definitions(${erf_lib_name} PUBLIC ERF_USE_NOAHMP)
     target_link_libraries_system(${erf_lib_name} PUBLIC NoahMP::noahmp)
   endif()
+
+  if(ERF_ENABLE_NOAHMP_MPMD)
+    target_compile_definitions(${erf_lib_name} PUBLIC ERF_USE_NOAHMP_MPMD)
+    set(AMReX_MPMD TRUE CACHE BOOL "Enable AMReX MPMD support" FORCE)
+  endif()
+
 
   ########################### GPU defs for KOKKOS #################################
   if(ERF_ENABLE_CUDA)
